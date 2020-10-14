2427bb499914e1097ab296b9819aaea3db0020ba
杨徐峰 <764539513@qq.com>
2020-10-14 21:17:39 +0800

    修改说明：Security学习提交

--- component-rbac/src/main/java/org/honeybee/rbac/config/SecurityConfig.java
+++ component-rbac/src/main/java/org/honeybee/rbac/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.honeybee.rbac.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.PrintWriter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,7 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("123456")
-                .roles("admin");
+                .roles("admin")
+                .and()
+                .withUser("honeybee")
+                .password("123456")
+                .roles("user");
     }
 
     @Override
@@ -33,12 +41,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/user/**").hasRole("user")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login.html")
+                .loginProcessingUrl("/doLogin")     //登录接口
+                .usernameParameter("username")  //登录用户参数名
+                .passwordParameter("password")  //登录密码参数名
+                .successHandler(((httpServletRequest, httpServletResponse, authentication) -> { //登录成功处理
+                    httpServletResponse.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(authentication.getPrincipal()));
+                    out.flush();
+                    out.close();
+                }))
+                .failureHandler(((httpServletRequest, httpServletResponse, e) -> {  //登录失败处理
+                    httpServletResponse.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(e.getMessage()));
+                    out.flush();
+                    out.close();
+                }))
+                .permitAll()        //放行相关url
+                .and()
+                .logout()
+//                .logoutUrl("/logout")     //登出接口
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))     //自定义登出接口
+                .logoutSuccessHandler(((httpServletRequest, httpServletResponse, authentication) -> {
+                    httpServletResponse.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString("登出成功"));
+                    out.flush();
+                    out.close();
+                }))
                 .permitAll()
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .exceptionHandling()    //未认证处理
+                .authenticationEntryPoint(((httpServletRequest, httpServletResponse, e) -> {
+                    httpServletResponse.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = httpServletResponse.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString("尚未登录,请先登录"));
+                    out.flush();
+                    out.close();
+                }));
+
     }
+
 }
