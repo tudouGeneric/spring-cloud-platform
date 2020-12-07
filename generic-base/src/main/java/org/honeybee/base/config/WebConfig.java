e3ff2a3355c8ead793ddeb476422ebfcdde976b3
yangxufeng <764539513@qq.com>
2020-12-07 09:36:05 +0800

    修改说明: 更新自定义静态文件接口配置; 增加ES远程扩展字典

--- generic-base/src/main/java/org/honeybee/base/config/WebConfig.java
+++ generic-base/src/main/java/org/honeybee/base/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -27,6 +28,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    /**
+     * 增加静态资源访问配置
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     /**
